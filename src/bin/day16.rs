bff46c90eb22b61e7fed28bc4b847931164f1c58
Claus <cshagen@hagens.ch>
2024-12-31 07:48:57 +0100

    recursive implementation of day 16

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -9,42 +9,49 @@ pub fn main() {
   //println!("Part 2: {}", part2(FILENAME));
 }
 fn part1(filename: &str) -> i64 {
-  let mut maze = Maze {
-    board: vec![],
-    start: Position { x: 0, y: 0 },
-    end: Position { x: 0, y: 0 },
-    vlen: 0,
-    hlen: 0,
-		cache: HashMap::new()
-  };
-  let dir: Direction = Direction::Right;
-
+  let mut maze = Maze::new();
+  
   maze = maze.parse(filename);
-	let result: Option<u32>;
-  (maze,result) = maze.walk(&Position { x: 999999, y: 999999 }, &dir, vec![]);
-	result.unwrap() as i64 -1
-
+  let result: Option<u64>;
+  (_, result, _) = maze.walk(&Position { x: 999999, y: 999999 }, &Direction::Right, vec![]);
+  result.unwrap() as i64 - 1
 }
 
 struct Maze {
-  board: Vec<Vec<i32>>,
+  board: Vec<Vec<i64>>,
   start: Position,
   end: Position,
   vlen: usize,
   hlen: usize,
-	cache: HashMap<(Position,Direction),u32>
+  cache: HashMap<(Position, Direction), u64>,
+	unvisited: Vec<(Position,Direction,u64)>
 }
 impl Maze {
+
+	fn new () -> Self {
+		Maze {
+			board: vec![],
+    start: Position { x: 0, y: 0 },
+    end: Position { x: 0, y: 0 },
+    vlen: 0,
+    hlen: 0,
+    cache: HashMap::new(),
+		unvisited: vec![]
+		}
+	}
+
   fn parse(mut self, filename: &str) -> Maze {
     for (y, line) in get_lines(filename).iter().enumerate() {
-      for (x, c) in line.chars().enumerate() {
-        if c == 'S' {
-          self.start = Position { x, y };
-        }
-        if c == 'E' {
-          self.end = Position { x, y };
-        }
-      }
+			for (x, c) in line.chars().enumerate() {
+				if c != '#' {
+        	if c == 'S' {
+          	self.start = Position::new(x,y) };
+        	}
+        	if c == 'E' {
+          	self.end = Position::new(x,y) };
+        	}
+				self.unvisited.push((Position::new(x,y),Direction::Right,i64::MAX));	
+			}
       self.board.push(line.chars().map(|c| if c == '#' { -1 } else { 0 }).collect());
     }
     self.hlen = self.board[0].len();
@@ -53,115 +60,164 @@ impl Maze {
   }
 
   // get the next legal steps from the current position
-  fn next_steps(&self, p: &Position, current_dir: Direction) -> Vec<(Position, Direction)> {
+  fn next_steps(&self, p: &Position, current_dir: Direction) -> (Vec<(Position, Direction)>) {
     let directions = [Direction::Right, Direction::Down, Direction::Left, Direction::Up];
     let mut result: Vec<(Position, Direction)> = vec![];
+    let mut loop_detected = false;
     for dir in directions {
       match dir {
         Direction::Right => {
-          if current_dir != Direction::Left && p.x < self.hlen - 1 && self.is_free(p.x + 1, p.y) {
-            result.push((Position { x: p.x + 1, y: p.y }, dir))
+          let new_pos = Position { x: p.x + 1, y: p.y };
+          if current_dir != Direction::Left && p.x < self.hlen - 1 && self.is_free(&new_pos) {
+            result.push((new_pos, dir));
           }
         }
         Direction::Down => {
-          if current_dir != Direction::Up && p.y < self.vlen - 1 && self.is_free(p.x, p.y + 1) {
-            result.push((Position { x: p.x, y: p.y +1 }, dir))
+          let new_pos = Position { x: p.x, y: p.y + 1 };
+          if current_dir != Direction::Up && p.y < self.vlen - 1 && self.is_free(&new_pos){
+            result.push((new_pos, dir))
           }
         }
         Direction::Left => {
-          if current_dir != Direction::Right && p.x > 0 && self.is_free(p.x - 1, p.y) {
-            result.push((Position { x: p.x - 1, y: p.y }, dir))
+          let new_pos = Position { x: p.x - 1, y: p.y };
+          if current_dir != Direction::Right && p.x > 0 && self.is_free(&new_pos) {
+            result.push((new_pos, dir))
           }
         }
         Direction::Up => {
-          if current_dir != Direction::Down && p.y > 0 && self.is_free(p.x, p.y -1) {
-            result.push((Position { x: p.x, y: p.y -1 }, dir))
+          let new_pos = Position { x: p.x, y: p.y - 1 };
+          if current_dir != Direction::Down && p.y > 0 && self.is_free(&new_pos) {
+            result.push((new_pos, dir))
           }
         }
       };
     }
-    result
+    (result)
   }
+
   // return the value at the position provided
-/*   fn get(&self, pos: Position) -> i32 {
+  fn get(&self, pos: Position) -> i64 {
     self.board[pos.y][pos.x]
-  } */
+  }
+
   // recursive: walk through the maze from position p, trying to find the shortest path
-  fn walk(mut self, p: &Position, dir: &Direction, visited: Vec<Position>) -> (Maze, Option<u32>) {
+  fn walk(mut self, p: &Position, dir: &Direction, visited: Vec<(Position, Direction)>) -> (Maze, Option<u64>, bool) {
     let pos: Position;
-		//println!("{:?}",p);
-		//println!("Size of visited: {}",visited.len());
+    let mut circle: bool = false;
+
     if p.x == 999999 && p.y == 999999 {
       pos = self.start.clone();
     } else {
       pos = p.clone();
     }
-		let	cached = self.cache.get(&(pos.clone(),dir.clone()));
-		if cached.is_some() {
-			println!("CACHE HIT");
-			let count = *cached.unwrap();
-			return (self,Some(count));
-		}
+
+    let cached = self.cache.get(&(pos.clone(), dir.clone()));
+    if cached.is_some() {
+      let count = *cached.unwrap();
+      return (self, Some(count),circle);
+    }
+
     if pos == self.end {
       println!("REACHED END");
-			self.cache.insert ((pos, *dir),1);
-      return (self, Some(1));
+      self.cache.insert((pos, *dir), 1);
+      return (self, Some(1),false);
     }
-    if visited.contains(&pos) {
-			//println!("loop: {:?}",pos);
-			return (self, None);
-    }
-    let next_steps = self.next_steps(&pos, *dir);
+		
+    let mut new_visited = visited.to_vec();
+    new_visited.push((pos.clone(), *dir));
+    let next_steps: Vec<(Position, Direction)>;
+    (next_steps, circle) = self.next_steps(&pos, *dir, &new_visited);
     if next_steps.len() == 0 {
-			//println!("stuck: {:?}",pos);
-      return (self,None);
+     /*  if !circle || !previous_circle {
+        self.cache.insert((pos, *dir), u64::MAX);
+      } */
+      return (self, None, circle);
     }
-    let mut min: u32 = 4294967292;
-		let mut min_dir = Direction::Right;
-		let mut new_visited = visited;
-    new_visited.push(pos.clone());
+    let mut min: u64 = u64::MAX;
+		let mut mincircle = false;
     for (next_pos, new_dir) in next_steps.iter() {
-			let count:Option<u32>;
-      (self,count) = self.walk(next_pos, new_dir, new_visited.to_vec());
-			if count.is_some() {
-				println!("-- Count: {}",count.unwrap());
-      	if count.unwrap() <= min {
-        	min = count.unwrap();
-					min_dir = *new_dir;
-      	}
-    	} else {println!("count is none")};
+      let count: Option<u64>;
+			let subcircle : bool;
+      (self, count, subcircle) = self.walk(next_pos, new_dir, new_visited.to_vec());
+      if count.is_some() && count.unwrap() < u64::MAX{
+				let mut val = count.unwrap();
+				//println!("{}",val);
+				if dir != new_dir {
+					val += 1000;
+				} 
+        if val < min {
+          min = val;
+					mincircle = subcircle;
+				} 
+    	}
 		}
-		if min < 4294967292 {
-			println! ("Min  with {}",  min);
-			if min_dir != *dir {
-				self.cache.insert((pos,*dir),min+1);
-				println!("cache insert");
-    	return (self,Some(min + 1001));
+		circle = circle || mincircle;
+    if min < u64::MAX { // we found at least 1 path to the end
+      let points = min + 1;
+      
+      if !circle  {
+        self.cache.insert((pos, *dir), points);
+      }
+      return (self, Some(points),circle);
+     } else { // there was no path to the end
+      if !circle  {
+        self.cache.insert((pos, *dir), u64::MAX);
+      }
+      return (self, None, circle);
+    } 
+  }
+	// Dijkstra shortest path search
+	fn scan (mut self, start_pos: Position, start_dir: Direction) -> i64 {
+		let mut pos = start_pos.clone();
+		let mut dir = start_dir;
+		let mut done = false;
+		self.board[start_pos.y][start_pos.x] = 0;
+		while !done {
+			remove_from_unvisited (pos);
+			let next_steps = self.next_steps(&pos, dir);
+			let mut min = u64::MAX;
+			let mut min_pos 
+			for (next_pos, next_dir) in next_steps {
+				let mut pts = self.get(pos);
+				if next_dir == dir {
+					pts += 1000;
+				} else {
+					pts += 1001;
+				}
+				if pts < self.board[next_pos.y][next_pos.x] {
+					self.board[next_pos.y][next_pos.x] = pts;
+				}
 			}
-			else {
-				self.cache.insert((pos,*dir),min+1);
-				println!("cache insert: {}",min+1);
-				return (self,Some(min+1));
+		}
+		0
+	}
+  fn is_free(&self, p: &Position) -> bool {
+    self.get(p.clone()) != -1
+  }
+	fn remove_from_unvisited(mut self, p: position) {
+		self.unvisited.iter().find()
+		for (i,pos) in self.unvisited.iter().enumerate() {
+			if pos == p {
+				self.unvisited.remove(i);
 			}
-		} else {
-		return (self,None);
+		}
 	}
 }
 
-  fn is_free(&self, x: usize, y: usize) -> bool {
-    self.board[y][x] != -1
-  }
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy,Hash)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 enum Direction {
   Right,
   Down,
   Left,
   Up,
 }
-#[derive(Debug, PartialEq, Eq, Clone,Hash)]
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 struct Position {
   x: usize,
   y: usize,
+}
+impl Position {
+	fn new (x: usize, y: usize) -> Self {
+		Position{x,y}
+	}
 }
--- src/bin/day21.rs
+++ src/bin/day21.rs
@@ -19,16 +19,16 @@ fn part1(filename: &str) -> i64 {
     let mut coderesult = 0;
     // Numberpad
     for c in code {
-			let path_options = get_moves_recursive(number_pos(prev_key_1), number_pos(*c)); // all the possible paths between the two buttons
+      let path_options = get_moves_recursive(number_pos(prev_key_1), number_pos(*c)); // all the possible paths between the two buttons
       let mut min_len = 2147483647;
       for path in path_options {
-			  // Dir Pad 1
+        // Dir Pad 1
         let mut move_groups_dir1: Vec<Vec<Direction>> = vec![];
         for m in path.into_iter() {
           move_groups_dir1.push(dirpad_moves(prev_key_2, m.clone()));
           prev_key_2 = m;
         }
-			  // Dir Pad 2
+        // Dir Pad 2
         let mut move_groups_dir2: Vec<Vec<Direction>> = vec![];
         for m in move_groups_dir1.concat().into_iter() {
           move_groups_dir2.push(dirpad_moves(prev_key_3, m.clone()));
@@ -40,17 +40,16 @@ fn part1(filename: &str) -> i64 {
         }
         prev_key_2 = Direction::Push;
         prev_key_3 = Direction::Push;
-			}
+      }
       coderesult += min_len;
       prev_key_1 = *c;
       prev_key_2 = Direction::Push;
       prev_key_3 = Direction::Push;
-			
     }
 
     let num = code[0..3].into_iter().collect::<String>().parse::<u64>().unwrap();
     result += num * coderesult;
-   }
+  }
   result as i64
 }
 // ----------------------------------------- part2()
@@ -61,14 +60,14 @@ fn part2(filename: &str) -> i64 {
   let mut result = 0;
   let mut game2 = Game2 { cache: HashMap::new() };
 
-	for code in codes.iter() {
-		let mut coderesult = 0;
+  for code in codes.iter() {
+    let mut coderesult = 0;
     // Numberpad
     for key in code {
-	    let path_options = get_moves_recursive(number_pos(prev_key_1), number_pos(*key)); // all the possible paths between the two buttons
+      let path_options = get_moves_recursive(number_pos(prev_key_1), number_pos(*key)); // all the possible paths between the two buttons
       let mut min_len = u64::MAX;
       for path in path_options {
-	      let mut move_groups: Vec<u64> = vec![];
+        let mut move_groups: Vec<u64> = vec![];
         for m in path.into_iter() {
           let steps: u64;
           (game2, steps) = game2.dir_pad_clicks(prev_key_2, m.clone(), 1);
@@ -81,7 +80,7 @@ fn part2(filename: &str) -> i64 {
           min_len = num;
         }
         prev_key_2 = Direction::Push;
-	    }
+      }
       coderesult += min_len;
       prev_key_1 = *key;
       prev_key_2 = Direction::Push;
@@ -104,16 +103,16 @@ struct Game2 {
   cache: HashMap<(u64, Direction, Direction), u64>,
 }
 impl Game2 {
-	// -----------------------------------------dir_pad_clicks()
+  // -----------------------------------------dir_pad_clicks()
   fn dir_pad_clicks(mut self, prev: Direction, key: Direction, level: u64) -> (Game2, u64) {
     let cached = self.cache.get(&(level, prev, key)).copied();
     let result: u64;
     if cached.is_some() && cached.unwrap() > 0 {
-	    return (self, cached.unwrap().clone());
+      return (self, cached.unwrap().clone());
     } else {
       if level == LEVELS {
         let steps_bottom = dirpad_moves(prev, key);
-        result = steps_bottom.len() as u64 ;
+        result = steps_bottom.len() as u64;
       } else {
         let mut steps2: u64;
         let path_options = dirpad_move_options(dir_pos(prev), dir_pos(key)); // all the possible paths between the two buttons
@@ -139,7 +138,7 @@ impl Game2 {
   }
 
   fn cache_add(mut self, level: u64, from: Direction, to: Direction, steps: u64) -> Game2 {
-	  self.cache.insert((level, from, to), steps.clone());
+    self.cache.insert((level, from, to), steps.clone());
     self
   }
 }
@@ -225,9 +224,9 @@ fn dirpad_move_options(from: (i64, i64), to: (i64, i64)) -> Vec<Vec<Direction>> 
   let mut result: Vec<Vec<Direction>> = vec![];
   let vdist = to.1 - from.1;
   let hdist = to.0 - from.0;
-	if hdist == 0 && vdist == 0 {
-		return vec![vec![Direction::Push]];
-	}
+  if hdist == 0 && vdist == 0 {
+    return vec![vec![Direction::Push]];
+  }
   if hdist > 0 {
     if hdist == 1 && vdist == 0 {
       result.push(vec![Direction::Right, Direction::Push]);
