e43b09495a343034a4f41e7e94d3890398364122
Claus <cshagen@hagens.ch>
2025-01-05 21:05:33 +0100

    day 23 part 2

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -3,7 +3,7 @@ const FILENAME: &'static str = "./data/d23-input.txt";
 
 pub fn main() {
   println!("Part 1: {}", part1(FILENAME));
-  //println!("Part 2: {}", part2(FILENAME));
+  println!("Part 2: {}", part2(FILENAME));
 }
 
 fn part1(filename: &str) -> i64 {
@@ -11,28 +11,76 @@ fn part1(filename: &str) -> i64 {
   let mut triples: Vec<(String, String, String)> = vec![];
   let mut map: Vec<(String, String)> = vec![];
   //parse
-	for line in lines {
+  for line in lines {
     let els = line.split_once('-').unwrap();
     map.push((els.0.to_string(), els.1.to_string()));
   }
-	// find matching triples
-	for (a, b) in map.iter() {
+  // find matching triples
+  for (a, b) in map.iter() {
     for c in find_values_for(&b, &map) {
-      let triple = (a.clone(),b.clone(),c.clone());// sort_triple((a, b, &c));
-			let sorted =sort_triple((a, b, &c));
-      if !(*a).eq(&c) 
-					&& starts_with_t((&triple.0,&triple.1,&triple.2))
-					&& !contains(&triples,&sorted) 
-					&& (map.contains(&(c.clone(),a.clone())) || map.contains(&(a.clone(),c.clone())))
-					{
-						println!("{:?}",sorted);
+      let triple = (a.clone(), b.clone(), c.clone()); // sort_triple((a, b, &c));
+      let sorted = sort_triple((a, b, &c));
+      if !(*a).eq(&c) && starts_with_t((&triple.0, &triple.1, &triple.2)) && !contains(&triples, &sorted) && (map.contains(&(c.clone(), a.clone())) || map.contains(&(a.clone(), c.clone()))) {
         triples.push(sorted);
       }
     }
   }
-	let result : Vec<&(String,String,String)> = triples.iter().collect();
+  let result: Vec<&(String, String, String)> = triples.iter().collect();
   result.len() as i64
-  
+}
+
+fn part2(filename: &str) -> i32 {
+  let lines: Vec<String> = get_lines(filename);
+  let mut groups: Vec<Vec<String>> = vec![];
+  let mut edges: Vec<(String, String)> = vec![];
+  //parse
+  for line in lines {
+    let (a, b) = line.split_once('-').unwrap();
+    groups.push(vec![a.to_string(), b.to_string()]);
+    edges.push((a.to_string(), b.to_string()));
+    edges.push((b.to_string(), a.to_string()));
+  }
+  // grow the groups
+  let mut targetsize = 3;
+  let mut done = false;
+  while !done {
+    let mut added = 0;
+    for group in groups.iter_mut() {
+      let mut addme: String = "".to_string();
+      for c in find_values_for(&group[0], &edges) {
+        let mut fit = true;
+        for b in group[1..].iter() {
+          if !edges.contains(&(c.clone(), b.clone())) {
+            fit = false;
+            break;
+          }
+        }
+        if fit {
+          addme = c;
+          break;
+        }
+      }
+      if addme != "" {
+        group.push(addme.clone());
+        added += 1;
+      } else {
+      }
+    }
+    if added == 0 {
+      done = true;
+    } else {
+      targetsize += 1;
+    }
+    groups = groups.iter().filter(|g| g.len() == targetsize - 1).map(|g| g.to_vec()).collect();
+  }
+  let longest: Vec<Vec<String>> = groups.iter().filter(|g| g.len() == targetsize - 1).map(|g| g.to_vec()).collect();
+  let mut result = longest[0].to_vec();
+  result.sort();
+  for s in result.iter() {
+    print!("{},", s)
+  }
+  println!("");
+  0
 }
 
 fn find_values_for(key: &str, map: &Vec<(String, String)>) -> Vec<String> {
@@ -43,22 +91,26 @@ fn contains(triples: &Vec<(String, String, String)>, t: &(String, String, String
   let a = t.0.to_string();
   let b = t.1.to_string();
   let c = t.2.to_string();
-  if triples.contains(&(a.clone(), b.clone(), c.clone())) || triples.contains(&(a.clone(), c.clone(), b.clone())) 
-	|| triples.contains(&(b.clone(), a.clone(), c.clone())) || triples.contains(&(b.clone(), c.clone(), a.clone())) 
-	|| triples.contains(&(c.clone(), a.clone(), b.clone())) || triples.contains(&(c.clone(), b.clone(), a.clone())) {
+  if triples.contains(&(a.clone(), b.clone(), c.clone()))
+    || triples.contains(&(a.clone(), c.clone(), b.clone()))
+    || triples.contains(&(b.clone(), a.clone(), c.clone()))
+    || triples.contains(&(b.clone(), c.clone(), a.clone()))
+    || triples.contains(&(c.clone(), a.clone(), b.clone()))
+    || triples.contains(&(c.clone(), b.clone(), a.clone()))
+  {
     true
   } else {
     false
   }
 }
-fn starts_with_t (triple: (&str,&str,&str)) -> bool{
-	let (a,b,c) = triple;
-	a.chars().nth(0).unwrap()  == 't' || b.chars().nth(0).unwrap() =='t' || c.chars().nth(0).unwrap()=='t'
+fn starts_with_t(triple: (&str, &str, &str)) -> bool {
+  let (a, b, c) = triple;
+  a.chars().nth(0).unwrap() == 't' || b.chars().nth(0).unwrap() == 't' || c.chars().nth(0).unwrap() == 't'
 }
-fn sort_triple (t:(&str, &str, &str)) -> (String,String,String) {
-	let mut v = vec![t.0,t.1,t.2];
-	v.sort();
-	//println!("{:?} / {:?}", t, v);
-	
-	(v[0].to_string(),v[1].to_string(),v[2].to_string())
+fn sort_triple(t: (&str, &str, &str)) -> (String, String, String) {
+  let mut v = vec![t.0, t.1, t.2];
+  v.sort();
+  //println!("{:?} / {:?}", t, v);
+
+  (v[0].to_string(), v[1].to_string(), v[2].to_string())
 }
--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -5,15 +5,15 @@ const FILENAME: &'static str = "./data/d24-input.txt";
 
 pub fn main() {
   println!("Part 1: {}", part1(FILENAME));
-  //println!("Part 2: {}", part2(FILENAME));
+  println!("Part 2: {}", part2(FILENAME));
 }
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
 enum Op {
   OR,
   AND,
   XOR,
 }
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Gate {
   in1: String,
   in2: String,
@@ -52,7 +52,7 @@ fn part1(filename: &str) -> i64 {
   let mut done = false;
 
   while !done {
-		done = true;
+    done = true;
     for g in gates.iter() {
       let executed: bool;
       (executed, vals) = exec(g, vals);
@@ -63,13 +63,87 @@ fn part1(filename: &str) -> i64 {
 
   let mut r = vals.into_iter().filter(|e| e.0.chars().nth(0).unwrap() == 'z').collect::<Vec<(String, i64)>>();
   r.sort();
-	println!("{:?}",r);
-	let result : i64= r.into_iter().map(|(_,n)|n).enumerate().fold(0,|acc,(i,b)| acc + b  * 2_i64.pow(i as u32));
-	
+  //println!("{:?}",r);
+  let result: i64 = r.into_iter().map(|(_, n)| n).enumerate().fold(0, |acc, (i, b)| acc + b * 2_i64.pow(i as u32));
+
   println!("{:?}", result);
   0
 }
 
+fn part2(filename: &str) -> &str {
+  let mut scan_gates = false;
+  let mut gates: Vec<Gate> = vec![];
+  let mut vals: HashMap<String, i64> = HashMap::new();
+  for line in get_lines(filename) {
+    if line.len() == 0 {
+      scan_gates = true;
+    } else if scan_gates {
+      let elts: Vec<&str> = line.split_whitespace().collect();
+      gates.push(Gate {
+        in1: elts[0].to_string(),
+        in2: elts[2].to_string(),
+        out: elts[4].to_string(),
+        op: match elts[1] {
+          "OR" => Op::OR,
+          "AND" => Op::AND,
+          "XOR" => Op::XOR,
+          _ => panic!("Invalid operation"),
+        },
+      });
+    } else {
+      let elts = line.split_once(":").unwrap();
+      vals.insert(elts.0.to_string(), elts.1.trim().parse::<i64>().unwrap());
+    }
+  }
+  //println!("======== INPUTS =======");
+  let mut icounter = 0;
+  for gate in gates.iter() {
+    if ['x', 'y'].contains(&gate.in1.chars().nth(0).unwrap()) {
+      //  println!("{} {:?} {} -> {}", gate.in1, gate.op, gate.in2, gate.out);
+      icounter += 1;
+    }
+    //println!("{:?}",gate);
+  }
+  //println!("======== PROCESSING =======");
+  let mut pcounter = 0;
+  for gate in gates.iter() {
+    if !['x', 'y'].contains(&gate.in1.chars().nth(0).unwrap()) && gate.out.chars().nth(0).unwrap() != 'z' {
+      //  println!("{} {:?} {} -> {}", gate.in1, gate.op, gate.in2, gate.out);
+      pcounter += 1
+    }
+  }
+  let mut rcounter = 0;
+  //println!("======== RESULTS =======");
+  for gate in gates.iter() {
+    if gate.out.chars().nth(0).unwrap() == 'z' {
+      //  println!("{} {:?} {} -> {}", gate.in1, gate.op, gate.in2, gate.out);
+      rcounter += 1;
+    }
+  }
+
+  println!("Gates: Input {}, Process {}, Output {}", icounter, pcounter, rcounter);
+  //let mut inputs : Vec<&str> = vec![];
+  //for i in 1..44 {
+  //	inputs.push()
+  //}
+/*   for input in [
+    "x01", "x02", "x03", "x04", "x05", "x06", "x07", "x08", "x09", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26", "x27", "x28",
+    "x29", "x30", "x31", "x32", "x33", "x34", "x35", "x36", "x37", "x38", "x39", "x40", "x41", "x42", "x43", "x44",
+  ] {
+    let res = find_path_to_next_output(input, &gates);
+
+    
+
+    //println!("{:?}",res);
+  } */
+	let result = find_target("x36", 2, &gates);
+	for g in result {
+		println!("{} {} ({:?}) {}",g.in1,g.in2,g.op,g.out);
+	}
+	//println!("{:?}", result);
+
+  "work in progress"
+}
 fn exec(g: &Gate, mut vals: HashMap<String, i64>) -> (bool, HashMap<String, i64>) {
   let r = vals.get(&g.out);
   if r.is_some() {
@@ -108,4 +182,53 @@ fn exec(g: &Gate, mut vals: HashMap<String, i64>) -> (bool, HashMap<String, i64>
   } else {
     return (false, vals);
   }
+}
+
+fn find_target(input: &str, max: usize, gates: &Vec<Gate>) -> Vec<Gate> {
+  let mut result: Vec<Gate> = vec![];
+  let mut backlog = find_gates_for_input(input, gates);
+  let mut counter = 0;
+
+  while backlog.len() > 0 {
+    let next_gate = backlog.splice(0..1, []).collect::<Vec<Gate>>()[0].clone();
+		result.push(next_gate.clone());
+    if next_gate.out.chars().nth(0).unwrap() == 'z' {
+      counter += 1;
+    }
+    if counter == max {
+      return result;
+    }
+    let mut next_gates = find_gates_for_input(&next_gate.out, gates);
+    backlog.append(&mut next_gates);
+  }
+
+  result
+}
+
+fn find_path_to_next_output(input: &str, gates: &Vec<Gate>) -> Gate {
+  let start = find_gate_for_input_and_op(input, "XOR", gates);
+  let middle1_name = start.out;
+  let middle1 = find_gate_for_input_and_op(&middle1_name, "XOR", gates);
+
+  println!("From {} to {}", input, middle1.out);
+
+  middle1.clone()
+}
+
+fn find_gates_for_input(input: &str, gates: &Vec<Gate>) -> Vec<Gate> {
+  gates.iter().filter(|g| g.in1.clone() == input || g.in2 == input).map(|g| g.clone()).collect()
+}
+fn find_gate_for_input_and_op(input: &str, op: &str, gates: &Vec<Gate>) -> Gate {
+  let oper = match op {
+    "AND" => Op::AND,
+    "XOR" => Op::XOR,
+    "OR" => Op::OR,
+    _ => panic!("wrong op"),
+  };
+  gates
+    .iter()
+    .filter(|g| (g.in1.clone() == input || g.in2 == input) && g.op == oper)
+    .map(|g| g.clone())
+    .collect::<Vec<Gate>>()[0]
+    .clone()
 }
